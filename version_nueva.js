c78f2084538a080919a9d540821fbc9132a108d5
andre gama del otero <andredelotero@gmail.com>
2021-07-27 16:48:58 -0300

    version 2.0

---
+++ version_nueva.js
@@ -0,0 +1,13 @@
+function mover(){
+    var segundos=document.querySelector(".segundos");
+    var minutos=document.querySelector(".minutos");
+    var horas=document.querySelector(".horas");
+    var date= new Date();
+    var s=date.getSeconds();
+    var m=date.getMinutes();
+    var h=date.getHours();
+    segundos.style.transform="rotate("+s*6+"deg)";
+    minutos.style.transform="rotate("+m*6+"deg)";
+    horas.style.transform="rotate("+((h*30)+((m*12)/30))+"deg)";
+    setInterval(function(){mover();}, 1000);
+}
